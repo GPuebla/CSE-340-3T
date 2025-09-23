b3267679aee1bf17f48109fe667373ab023a6fac
gpuebla <gabrielpuebla16@gmail.com>
2025-09-23 17:07:48 -0300

    Adding "api" route pref

--- server.js
+++ server.js
@@ -11,10 +11,14 @@ const { swaggerUi, specs } = require('./swagger');
 dotenv.config();
 const app = express();
 
+const routes = require('./routes/index');
+
 app.use(cors()); 
 
 app.use(express.json());
 app.use('/', routes);
+
+app.use('/api', routes); 
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
