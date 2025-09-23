b4878d36c72d4c7fa3dc033c401adc1101af669c
gpuebla <gabrielpuebla16@gmail.com>
2025-09-23 16:36:31 -0300

    Adding middleware to enable CORS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongodb = require('./data/database');
 const dotenv = require('dotenv');
 const routes = require('./routes');
+const cors = require('cors');
 
 const { swaggerUi, specs } = require('./swagger');
 
@@ -9,6 +10,8 @@ const { swaggerUi, specs } = require('./swagger');
 
 dotenv.config();
 const app = express();
+
+app.use(cors()); 
 
 app.use(express.json());
 app.use('/', routes);
